204f68c9ecb53f355d3cfd124316e5f257895851
AdrianaLechin <59151037+AdrianaLechin@users.noreply.github.com>
2019-12-23 12:14:56 -0500

    made grid with colors

--- designs.js
+++ designs.js
@@ -1,10 +1,30 @@
 // Select color input
 // Select size input
-
+var height,width,color;
 // When size is submitted by the user, call makeGrid()
-
-function makeGrid() {
+$('#sizePicker').submit(function(event){
+  event.preventDefault();
+  height =$('#inputHeight').val();
+  width =$('#inputWidth').val();
+  makeGrid(height, width);
+})
+function makeGrid(n,m) {
+    $('tr').remove();
 
 // Your code goes here!
+    for (var h= 1; i<=n; h++){
+      $('#pixelCanvas').append('<tr id= table'+ h + '></tr>');
+      for (var w= 1; w <= m; w++){
+        $('#table'+ h).append('<td></td');
+      }
+    }
+      $('td').clicked(function pickedColor() {
+        color = $('#colorPicker').val();
+        if ($(click).attribute('style')){
+          $(click).removeAttribute('style')
+        }else{
+          $(click).attribute('style', 'background-color:'+ color);
+        }
 
+      })
 }
