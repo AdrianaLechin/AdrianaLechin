0eeb0c8b45d5d66a4b79676aca6b8fbfd77794a4
AdrianaLechin <59151037+AdrianaLechin@users.noreply.github.com>
2019-12-23 20:40:37 -0500

    added notes and updated variables names

--- designs.js
+++ designs.js
@@ -1,29 +1,28 @@
+var height, width, color;
 
-var height,width,color;
-
-$('#sizePicker').submit(function(event){
+$("#sizePicker").submit(function(event) {
   event.preventDefault();
-  height =$('#inputHeight').val(); //num of squares representing height of grid
-  width =$('#inputWidth').val(); // num of squares representing width of grid
+  height = $("#inputHeight").val(); // height of grid
+  width = $("#inputWidth").val(); // width of grid
   makeGrid(height, width);
-})
-function makeGrid(n,m) {
-    $('tr').remove();
+});
 
-// h= height w= width
-    for (var h= 1; h<=n; h++){
-      $('#pixelCanvas').append('<tr id= table'+ h + '></tr>');
-      for (var w= 1; w <= m; w++){
-        $('#table'+ h).append('<td></td');
-      }
+function makeGrid(numOfCols, numOfRows) {
+  $("tr").remove();
+  // loop and build grid
+  for (var curCol = 1; curCol <= numOfCols; curCol++) {
+    $("#pixelCanvas").append("<tr id= table" + curCol + "></tr>");
+    for (var curRow = 1; curRow <= numOfRows; curRow++) {
+      $("#table" + curCol).append("<td></td");
     }
-      $('td').click(function pickedColor() {
-        color = $('#colorPicker').val();
-        if ($(this).attr('style')){
-          $(this).removeAttribute('style')
-        }else{
-          $(this).attr('style', 'background-color:'+ color);
-        }
-
-      })
+  }
+  // add click handler
+  $("td").click(function pickedColor() {
+    color = $("#colorPicker").val();
+    if ($(this).attr("style")) {
+      $(this).removeAttribute("style");
+    } else {
+      $(this).attr("style", "background-color:" + color);
+    }
+  });
 }
